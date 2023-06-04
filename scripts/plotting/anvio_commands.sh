dca815ad6e6de72d5f3ff81cef352b9e5c3139ac
Jay Håkansson <jay.hakansson.4449@student.uu.se>
2023-06-04 16:52:29 +0200

    command for running anvio

---
+++ scripts/plotting/anvio_commands.sh
@@ -0,0 +1,5 @@
+anvi-interactive -d bin_amr_presence.txt \
+                    -p checkm_profile.db \
+                    --title "AMR" \
+                    --tree bacteria.tree \
+                    --manual
